4a939e4917d583c105e446966ce0f899ccab8401
vladliena <sotnikovavladlena@gmail.com>
2022-12-18 10:53:01 +0200

    First task done

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,16 @@
-const numbersArray = [16,-37,54,-4,72,-56,47,4, -16,25,-37,46,4,-51,27,-63,4,-54,76,-4,12,-35,4,47]
+// Task 1
+
+const numbersArray = [16, -37, 54, -4, 72, -56, 47, 4, -16, 25, -37, 46, 4, -51, 27, -63, 4, -54, 76, -4, 12, -35, 4, 47];
+let result = 0;
+let value = [];
+
+for (let i = 0; i < numbersArray.length; i++) {
+    if (numbersArray[i] > 0) {
+        result += numbersArray[i];
+        value.push(numbersArray[i]);
+    }
+}
+console.log(`Сумма : ${result} Количество: ${value.length}`)
+
+//Task 2
+
