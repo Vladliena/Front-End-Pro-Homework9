ae7d74b41a631a001fdf48b009a2cb1812973261
vladliena <sotnikovavladlena@gmail.com>
2022-12-18 11:58:02 +0200

    deleted debugger

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,8 @@
-// Task 1
 
 const numbersArray = [16, -37, 54, -4, 72, -56, 47, 4, -16, 25, -37, 46, 4, -51, 27, -63, 4, -54, 76, -4, 12, -35, 4, 47];
+
+// Task 1
+
 let result = 0;
 let value = [];
 
@@ -13,4 +15,15 @@ for (let i = 0; i < numbersArray.length; i++) {
 console.log(`Сумма : ${result} Количество: ${value.length}`)
 
 //Task 2
+
+let minimalNumber = numbersArray[0];
+
+
+for (let i = 0; i < numbersArray.length; i++){
+    if (numbersArray[i] < minimalNumber)
+        minimalNumber = numbersArray[i]
+}
+console.log(minimalNumber)
+
+//Task 3
 
